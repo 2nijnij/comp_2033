ceea3321aa716bc9985d3ea8052511f715155cb0
Yejin Kim <kimyy049@mymail.unisa.edu.au>
2023-10-07 23:25:14 +1030

    Add a main method

--- src/ca1/task3/ArrayCount.java
+++ src/ca1/task3/ArrayCount.java
@@ -1,10 +1,10 @@
 package ca1.task3;
 
 public class ArrayCount {
-	public int count(String[] array, String target) {
+	public static int count(int[] numbers, int target) {
 		int count = 0;
 		
-		for(String num : array) {
+		for(int num : numbers) {
 			if (num == target) {
 				count++;
 			}
@@ -15,6 +15,11 @@ public class ArrayCount {
 	
 	public static void main(String[] args) {
 		
+		int[] numbers = {1, 2, 3, 4, 3, 5, 3};
+		int target = 3;
 		
+		int result = count(numbers, target);
+		
+		System.out.println("The targe " + target + " appears " + result + " times in the array !");
 	}
 }
