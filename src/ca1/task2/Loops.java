f85d5cc11c797ead3075c3844b76da8cb2e01da6
Yejin Kim <kimyy049@mymail.unisa.edu.au>
2023-10-07 23:09:42 +1030

    Add main method and comments

--- src/ca1/task2/Loops.java
+++ src/ca1/task2/Loops.java
@@ -1,5 +1,9 @@
 package ca1.task2;
 
+
+// implement the printWords method within a loop
+// set the parameter to determine the total number of times the loop should run
+
 public class Loops {
 	public void printWords(int numberOfTimes) {
 		for (int i = 0; i < numberOfTimes; i++) {
@@ -7,8 +11,14 @@ public class Loops {
 		}
 	}
 	
+	
+//Add a 'main' method	
 	public static void main(String[] args) {
 		
+		Loops loops = new Loops();
+		
+// Call the printWords method for verifying the implementation
+		loops.printWords(3);
 
 	}
 }
