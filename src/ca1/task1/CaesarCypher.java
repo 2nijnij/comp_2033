fe57c2e5bf7218d5e8f3582dba636122a2acd85a
Yejin Kim <kimyy049@mymail.unisa.edu.au>
2023-10-07 22:19:07 +1030

    encode method

--- src/ca1/task1/CaesarCypher.java
+++ src/ca1/task1/CaesarCypher.java
@@ -3,7 +3,14 @@ package ca1.task1;
 public class CaesarCypher {
 
 	public char[] encode(char[] input, int offset) {
-		return null;
+        for (int i = 0; i < input.length; i++) {
+            char c = input[i];
+            if (Character.isLetter(c)) {
+                char base = Character.isLowerCase(c) ? 'a' : 'A';
+                input[i] = (char) (base + (c - base + offset) % 26);
+            }
+        }
+        return input;
 	}
 	
 	public char[] decode(char[] input, int offset) {
