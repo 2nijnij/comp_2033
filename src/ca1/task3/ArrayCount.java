96b701d053ff44f583600d7de72eccd8ba66ef50
Yejin Kim <kimyy049@mymail.unisa.edu.au>
2023-10-07 23:28:16 +1030

    Add comments

--- src/ca1/task3/ArrayCount.java
+++ src/ca1/task3/ArrayCount.java
@@ -1,9 +1,12 @@
 package ca1.task3;
 
+// Use static for the count method
 public class ArrayCount {
 	public static int count(int[] numbers, int target) {
+
+// Initialize valuable as 0
 		int count = 0;
-		
+
 		for(int num : numbers) {
 			if (num == target) {
 				count++;
@@ -12,7 +15,9 @@ public class ArrayCount {
 		
 		return count;
 	}
+
 	
+// Add a main method
 	public static void main(String[] args) {
 		
 		int[] numbers = {1, 2, 3, 4, 3, 5, 3};
