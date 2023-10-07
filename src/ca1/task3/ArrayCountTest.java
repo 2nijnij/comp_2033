f8b849c41b1866bb29f641cfd133f9d06b6a9466
Yejin Kim <kimyy049@mymail.unisa.edu.au>
2023-10-07 23:38:23 +1030

    correct typo

--- src/ca1/task3/ArrayCountTest.java
+++ src/ca1/task3/ArrayCountTest.java
@@ -16,15 +16,15 @@ public class ArrayCountTest {
 	// Situation 2 (Target appears only once in Array)
 	    void testCountTargetOnceInArray() {
 	        int[] array = {1, 2, 3, 4, 5};
-	        int target = 3;
+	        int target = 5;
 	        int result = ArrayCount.count(array, target);
 	        assertEquals(1, result, "Count of target appearing in this array only once, should be 1");
 	    }
 	    
 	// Situation 3 (Target appears multiple times in Array)
 	    void testCountTargetMultipleTimesInArray() {
-	        int[] array = {1, 2, 3, 4, 3, 5, 3};
-	        int target = 3;
+	        int[] array = {1, 2, 3, 4, 5, 5, 3, 5};
+	        int target = 5;
 	        int result = ArrayCount.count(array, target);
 	        assertEquals(3, result, "Count of target appearing multiple times in this array, should be 3");
 	    }
