e39a8a894b4c3b392baf102f04cd18720d4d5416
Yejin Kim <kimyy049@mymail.unisa.edu.au>
2023-10-07 23:01:54 +1030

    method for printWords

--- src/ca1/task2/Loops.java
+++ src/ca1/task2/Loops.java
@@ -2,6 +2,13 @@ package ca1.task2;
 
 public class Loops {
 	public void printWords(int numberOfTimes) {
+		for (int i = 0; i < numberOfTimes; i++) {
+			System.out.println("words");
+		}
+	}
+	
+	public static void main(String[] args) {
 		
+
 	}
 }
