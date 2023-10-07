01eca8df578e4b11d3ed31f69654c8f8632b9829
Yejin Kim <kimyy049@mymail.unisa.edu.au>
2023-10-07 23:33:46 +1030

    Add comments for situations

--- src/ca1/task3/ArrayCountTest.java
+++ src/ca1/task3/ArrayCountTest.java
@@ -4,5 +4,11 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 public class ArrayCountTest {
-
+	
+	// Situation 1 (Target not in Array)
+	
+	
+	// Situation 2 (Target appears only once in Array)
+	
+	// Situation 3 (Target appears multiple times in Array)
 }
